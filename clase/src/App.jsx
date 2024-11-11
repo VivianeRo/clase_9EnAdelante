4c5e76de39d8dd17d872693267eeea838e3602d7
Viviane Rodriguez <vivianerg84@gmail.com>
2024-11-11 14:54:53 -0300

    comit

--- clase/src/App.jsx
+++ clase/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import { Card } from './componets/Card'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <div>
        <h1>Funcionando</h1>
+       
     </div>
   )
 }
